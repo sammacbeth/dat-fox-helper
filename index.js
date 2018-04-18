1db46b293baff38537849ccfab1c8db8828cb52e
Sam Macbeth <sam@macbeth.cc>
2018-04-18 17:14:13 +0200

    Add rest of DatArchive class methods

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 const process = require('process')
+const fs = require('fs');
 const DatGateway = require('dat-gateway');
 const browser = require('./browser');
 const DatArchive = require('node-dat-archive')
@@ -11,19 +12,23 @@ const gateway = new DatGateway({
     maxAge:  10 * 60 * 1000,
 });
 
-const archives = new Map();
+const libraryDir = './library';
+if (!fs.existsSync(libraryDir)) {
+    fs.mkdirSync(libraryDir);
+}
+const library = new Map();
 
 function getArchive(url) {
-    if (!archives.has(url)) {
+    if (!library.has(url)) {
         const path = url.replace('dat://', '');
-        archives.set(url, new DatArchive(url,  {
-            localPath: `./archives/${path}`,
+        library.set(url, new DatArchive(url,  {
+            localPath: `${libraryDir}/${path}`,
             datOptions: {
                 latest: true,
             }
         }));
     }
-    return archives.get(url);
+    return library.get(url);
 }
 
 const handlers = {
@@ -32,11 +37,22 @@ const handlers = {
     startGateway: ({ port=3000 }) => {
         return gateway.listen(port);
     },
+    // DatArchive static methods
     resolveName: (message) => DatArchive.resolveName(message.name),
+    // DatArchive class methods
     getInfo: (message) => getArchive(message.url).getInfo(message.opts),
     stat: (message) => getArchive(message.url).stat(message.path, message.opts),
     readdir: (message) => getArchive(message.url).readdir(message.path, message.opts),
     history: (message) => getArchive(message.url).history(message.opts),
+    readFile: ({ url, path, opts }) => getArchive(url).readFile(path, opts),
+    writeFile: ({ url, path, data, opts }) => getArchive(url).writeFile(path, data, opts),
+    mkdir: ({ url, path }) => getArchive(url).mkdir(path),
+    unlink: ({ url, path }) => getArchive(url).unlink(path),
+    rmdir: ({ url, path, opts }) => getArchive(url).rmdir(path, opts),
+    diff: ({ url, opts }) => getArchive(url).diff(opts),
+    commit: ({ url }) => getArchive(url).commit(),
+    revert: ({ url }) => getArchive(url).revert(),
+    download: ({ url, path, opts }) => getArchive(url).download(path, opts),
 }
 
 browser.onMessage.addListener((message) => {
